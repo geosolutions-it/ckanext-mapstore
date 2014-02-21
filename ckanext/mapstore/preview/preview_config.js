1175ed00f02945681dd90c66463b0075283987b5
tobia <tobia.dipisa@geo-solutions.it>
2014-02-21 13:12:09 +0100

    Cleaning up the preview configuration removing the optional properties.

--- ckanext/mapstore/preview/preview_config.js
+++ ckanext/mapstore/preview/preview_config.js
@@ -2,9 +2,5 @@ var preview_config = {
 	viewerConfigName: "preview",
 	viewerPath: "/viewer",
 	composerPath: "/composer",
-    mapStoreBaseURL: "http://192.168.1.114/mapstore",
-	backgroundData: {
-		baseMapId: "387",
-		geostoreBaseUrl: "http://192.168.1.110:8080/geostore/rest/"
-	}
+    mapStoreBaseURL: "http://192.168.1.114/mapstore"
 }
