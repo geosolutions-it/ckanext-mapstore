69fb467e045ad1c9f433b71f985722415e760e1f
tobia <tobia.dipisa@geo-solutions.it>
2015-03-30 18:48:15 +0200

    Small fixes for preview configuration

--- ckanext/mapstore/preview/preview_config.js
+++ ckanext/mapstore/preview/preview_config.js
@@ -1,10 +1,11 @@
 var preview_config = {
-	viewerConfigName: "preview",
-	viewerPath: "embedded",
-	composerPath: "composer",
-        mapStoreBaseURL: "http://localhost:8080/mapstore",
-	basketStatus: false,
-	storageMethod: "sessionstorage",
-	forceLocaleTo: "en",
-	storeSize: 5000
+             viewerConfigName: "preview",
+             viewerPath: "/viewer",
+             composerPath: "/composer",
+             mapStoreBaseURL: "http://localhost:8080/mapstore",
+             basketStatus: false,
+             storageMethod: "sessionstorage",
+             forceLocaleTo: "en",
+             storeSize: 5000
 }
+
